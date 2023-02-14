87381a5fadc30b48dd693e990801ed81be67a8d1
Yordi_DA <30624262+Devil-boy21@users.noreply.github.com>
2023-02-14 13:50:42 -0500

    Add files via upload

---
+++ WikitudeStudioAndroidAppTemplate/app/src/main/assets/project.js
@@ -0,0 +1,266 @@
+// JSON file describing the AR-experience for offline usage
+var projectJSONOffline =
+{
+    "isCloud": false,
+    "tcId": "63e5b05d9c6953004f9b581f",
+    "name": "Dragon",
+    "targets": [
+        {
+            "name": "Tyrannosaurus Rex",
+            "augmentations": [
+                {
+                    "type": "Button",
+                    "height": 0.3,
+                    "uri": "augmentation-assets/912266743_Tyrannosaurus_Rex/63e5bb889c6953004f9b5864/image.png",
+                    "targetId": "63e5ba79560fae00637d8e15",
+                    "minSDK": "6.1.0",
+                    "modDat": 1676000522501,
+                    "creDat": 1676000136237,
+                    "properties": {
+                        "clickUrl": "https://www.facebook.com/https://www.facebook.com/",
+                        "name": "button_1",
+                        "forceNativeBrowser": "false",
+                        "ignoreInAR": false,
+                        "opacity": 1,
+                        "rotate": {
+                            "x": 0,
+                            "y": 0,
+                            "z": 0
+                        },
+                        "scale": {
+                            "x": 0.5,
+                            "y": 0.5,
+                            "z": 0.5
+                        },
+                        "translate": {
+                            "x": -0.5338342994611754,
+                            "y": 0.4072352377733852,
+                            "z": 0
+                        },
+                        "zOrder": 3
+                    },
+                    "fileSize": 3903,
+                    "id": "63e5bb889c6953004f9b5864"
+                },
+                {
+                    "type": "Button",
+                    "height": 0.3,
+                    "uri": "augmentation-assets/912266743_Tyrannosaurus_Rex/63e5bbaf9c6953004f9b5866/image.png",
+                    "targetId": "63e5ba79560fae00637d8e15",
+                    "minSDK": "6.1.0",
+                    "modDat": 1676000522500,
+                    "creDat": 1676000175246,
+                    "properties": {
+                        "clickUrl": "mailto:mailto:",
+                        "name": "button_2",
+                        "forceNativeBrowser": "false",
+                        "ignoreInAR": false,
+                        "opacity": 1,
+                        "rotate": {
+                            "x": 0,
+                            "y": 0,
+                            "z": 0
+                        },
+                        "scale": {
+                            "x": 0.5,
+                            "y": 0.5,
+                            "z": 0.5
+                        },
+                        "translate": {
+                            "x": -0.3452256140671607,
+                            "y": 0.39843671174203965,
+                            "z": 0.0014410536966380086
+                        },
+                        "zOrder": 2
+                    },
+                    "fileSize": 7781,
+                    "id": "63e5bbaf9c6953004f9b5866"
+                },
+                {
+                    "type": "Button",
+                    "height": 0.3,
+                    "uri": "augmentation-assets/912266743_Tyrannosaurus_Rex/63e5bc0d9c6953004f9b5868/image.png",
+                    "targetId": "63e5ba79560fae00637d8e15",
+                    "minSDK": "6.1.0",
+                    "modDat": 1676000522505,
+                    "creDat": 1676000269417,
+                    "properties": {
+                        "clickUrl": "http://wikitude.com",
+                        "name": "button_3",
+                        "forceNativeBrowser": "false",
+                        "ignoreInAR": false,
+                        "opacity": 1,
+                        "rotate": {
+                            "x": 0,
+                            "y": 0,
+                            "z": 0
+                        },
+                        "scale": {
+                            "x": 0.5,
+                            "y": 0.5,
+                            "z": 0.5
+                        },
+                        "translate": {
+                            "x": -0.14826263450832144,
+                            "y": 0.39896006421687275,
+                            "z": 0
+                        },
+                        "zOrder": 1
+                    },
+                    "fileSize": 6308,
+                    "id": "63e5bc0d9c6953004f9b5868"
+                },
+                {
+                    "type": "ImageDrawable",
+                    "height": 0.3,
+                    "uri": "augmentation-assets/912266743_Tyrannosaurus_Rex/63e5bc7f9c6953004f9b586a/image.jpeg",
+                    "targetId": "63e5ba79560fae00637d8e15",
+                    "minSDK": "6.1.0",
+                    "modDat": 1676001100950,
+                    "creDat": 1676000383628,
+                    "properties": {
+                        "name": "IMG_0395",
+                        "forceNativeBrowser": "false",
+                        "ignoreInAR": false,
+                        "opacity": 1,
+                        "rotate": {
+                            "x": 0,
+                            "y": 0,
+                            "z": 0
+                        },
+                        "scale": {
+                            "x": 1,
+                            "y": 1,
+                            "z": 1
+                        },
+                        "translate": {
+                            "x": -0.365252843796105,
+                            "y": 0.2025816758540826,
+                            "z": 0.0023412599849934046
+                        },
+                        "zOrder": 0
+                    },
+                    "fileSize": 1724061,
+                    "id": "63e5bc7f9c6953004f9b586a"
+                },
+                {
+                    "type": "Model3D",
+                    "uri": "augmentation-assets/912266743_Tyrannosaurus_Rex/63e5bf229c6953004f9b586c/model.wt3",
+                    "targetId": "63e5ba79560fae00637d8e15",
+                    "minSDK": "6.1.0",
+                    "modDat": 1676061449892,
+                    "creDat": 1676001058289,
+                    "properties": {
+                        "name": "chik",
+                        "mtlUrl": "https://s3-eu-west-1.amazonaws.com/target-manager-live/76b89160f870043464f8b872b5fa77cf/63e5b05d9c6953004f9b581f/63e5ba79560fae00637d8e15/studio/wE6qaVoK/model.mtl",
+                        "isOccluder": false,
+                        "ignoreInAR": false,
+                        "rotate": {
+                            "x": 90,
+                            "y": 0,
+                            "z": 900
+                        },
+                        "translate": {
+                            "x": -0.13107335738282763,
+                            "y": -0.3501663331312207,
+                            "z": 0.03955793683977912
+                        },
+                        "scale": {
+                            "x": 0.0009,
+                            "y": 0.0009,
+                            "z": 0.0009
+                        }
+                    },
+                    "fileSize": 12964480,
+                    "id": "63e5bf229c6953004f9b586c"
+                },
+                {
+                    "type": "Model3D",
+                    "uri": "augmentation-assets/912266743_Tyrannosaurus_Rex/63e5cc069c6953004f9b5873/model.wt3",
+                    "targetId": "63e5ba79560fae00637d8e15",
+                    "minSDK": "6.1.0",
+                    "modDat": 1676061449890,
+                    "creDat": 1676004358450,
+                    "properties": {
+                        "name": "rata",
+                        "mtlUrl": "https://s3-eu-west-1.amazonaws.com/target-manager-live/76b89160f870043464f8b872b5fa77cf/63e5b05d9c6953004f9b581f/63e5ba79560fae00637d8e15/studio/eEpz35jz/model.mtl",
+                        "isOccluder": false,
+                        "ignoreInAR": false,
+                        "rotate": {
+                            "x": 0,
+                            "y": 0,
+                            "z": 0
+                        },
+                        "scale": {
+                            "x": 0.0009,
+                            "y": 0.0009,
+                            "z": 0.0009
+                        },
+                        "translate": {
+                            "x": 0.4192744314881524,
+                            "y": -0.222568054356234,
+                            "z": 0.004412033486252553
+                        }
+                    },
+                    "fileSize": 141412,
+                    "id": "63e5cc069c6953004f9b5873"
+                }
+            ]
+        }
+    ],
+    "type": "ImageTargetCollection",
+    "wtcFiles": [
+        {
+            "url": "augmentation-trackers/6.0/tracker.wtc",
+            "nrOfTargets": 1,
+            "version": "6.0",
+            "creDat": 1675999868696
+        },
+        {
+            "url": "augmentation-trackers/6.0/tracker.wtc",
+            "nrOfTargets": 1,
+            "version": "6.1",
+            "creDat": 1675999868696
+        },
+        {
+            "url": "augmentation-trackers/6.0/tracker.wtc",
+            "nrOfTargets": 1,
+            "version": "6.2",
+            "creDat": 1675999868696
+        },
+        {
+            "url": "augmentation-trackers/6.0/tracker.wtc",
+            "nrOfTargets": 1,
+            "version": "7.0",
+            "creDat": 1675999868696
+        },
+        {
+            "url": "augmentation-trackers/6.0/tracker.wtc",
+            "nrOfTargets": 1,
+            "version": "7.1",
+            "creDat": 1675999868696
+        },
+        {
+            "url": "augmentation-trackers/6.0/tracker.wtc",
+            "nrOfTargets": 1,
+            "version": "7.2",
+            "creDat": 1675999868696
+        },
+        {
+            "url": "augmentation-trackers/6.0/tracker.wtc",
+            "nrOfTargets": 1,
+            "version": "8.0",
+            "creDat": 1675999868696
+        },
+        {
+            "url": "augmentation-trackers/6.0/tracker.wtc",
+            "nrOfTargets": 1,
+            "version": "latest",
+            "creDat": 1675999868696
+        }
+    ],
+    "settings": {
+        "tracker": {}
+    }
+}
+;
